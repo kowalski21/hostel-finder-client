e3d7acde76f737cb19b15d7862216335a59fe2d5
Kowalski Devops <douglasbiomed3@gmail.com>
2024-07-17 13:34:31 +0000

    index page showing hostels

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,68 @@
 import LinkButton from "@/components/reusable/LinkButton";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { Loader } from "rsuite";
+import React, { useEffect, useState } from "react";
+import { Input, Loader } from "rsuite";
+import Link from "next/link";
+import { useAuthUser } from "@/store/auth";
+import { useAuthHook } from "@/hooks/auth";
+import { useHostels } from "@/hooks/hostels";
+import HostelCard from "@/components/dashboard/card/HostelCard";
 
 const Home = () => {
+  const { isLoading: loading, data: userInfo } = useAuthHook();
   const router = useRouter();
+  const user = useAuthUser();
+  const [search, setSearch] = useState("");
+
+  const [query, setQuery] = useState({ fields: "*,manager.id,manager.avatar" });
+
+  const { isLoading, data } = useHostels(["HostelsSearch", query], query);
 
   useEffect(() => {
-    router.push("/dashboard");
-  }, []);
+    let payload = { fields: "*,manager.id,manager.avatar" };
+
+    if (search) {
+      payload.search = search;
+    }
+
+    setQuery(payload);
+  }, [search]);
+
+  if (userInfo) {
+    return (
+      <div className="container mt-5">
+        <div className="row mt-5">
+          <div className="col text-center">
+            <h3 className="" style={{ fontSize: "70px" }}>
+              Hostel Finder
+            </h3>
+            <p className="text-muted">Find, Book a Hostel with ease</p>
+            <div className="separator text-dark mb-5"></div>
+            <button className="btn btn-dark btn-sm me-2">Login</button>
+            <button className="btn btn-dark btn-sm">Dashboard</button>
+          </div>
+        </div>
+        <div className="row mt-5">
+          {/* {JSON.stringify({ query })} */}
+          <Input value={search} onChange={(val) => setSearch(val)} placeholder="Search Hostel By Name" size="lg" />
+        </div>
+        <div className="row mt-5">
+          {isLoading && <Loader center vertical />}
+          {data?.data.map((hostel) => {
+            return (
+              <div className="col-md-3 mb-5" key={hostel.id}>
+                <HostelCard hostel={hostel} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container mt-5">
       <Loader center vertical />
-      {/* <LinkButton to="/auth/login">Login</LinkButton>
-      <LinkButton to="/signup">Sign Up</LinkButton>
-      <LinkButton to="/dashboard">Dashboard</LinkButton> */}
     </div>
   );
 };
