c50a32978330990da4c583d3feee607d34ba077a
Kowalski Devops <douglasbiomed3@gmail.com>
2024-09-02 23:59:57 +0000

    fixed asset url - corrected by removing /

--- src/lib/asset.js
+++ src/lib/asset.js
@@ -1,6 +1,6 @@
 export const assetUrl = (assetId) => {
   if (assetId) {
-    return `${process.env.NEXT_PUBLIC_API_URL}/assets/${assetId}`;
+    return `${process.env.NEXT_PUBLIC_API_URL}assets/${assetId}`;
   } else {
     return `/media/avatars/300-3.jpg`;
   }
